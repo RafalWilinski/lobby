6059c4a282330c3f8d93fa6542cf971b3e1a5029
Rafal Wilinski <raf.wilinski@gmail.com>
2017-12-05 14:20:01 +0100

    Add /dashboard frontend

--- components/dashboard/index.js
+++ components/dashboard/index.js
@@ -1,7 +1,109 @@
 import React from "react";
-import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import { Layout, Menu, Breadcrumb, Icon, Table, Row, Col } from "antd";
 
 const { Header, Content, Footer, Sider } = Layout;
+
+const topicColumns = [
+  {
+    title: "Nazwa",
+    dataIndex: "name",
+    key: "name",
+    render: text => <a href="#">{text}</a>
+  },
+  {
+    title: "Aplikanci",
+    dataIndex: "applicants",
+    key: "applicants"
+  },
+  {
+    title: "Akcja",
+    key: "action",
+    render: (text, record) => (
+      <span>
+        <a style={{ margin: "0 10px" }} href="#">
+          Usuń
+        </a>
+        <a style={{ margin: "0 10px" }} href="#">
+          Edytuj
+        </a>
+        <a style={{ margin: "0 10px" }} href="#">
+          Przeglądaj aplikacje
+        </a>
+      </span>
+    )
+  }
+];
+
+const applyColumns = [
+  {
+    title: "Nazwa tematu",
+    dataIndex: "topicName",
+    key: "topicName"
+  },
+  {
+    title: "Nazwa Stanowiska",
+    dataIndex: "positionName",
+    key: "positionName"
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+    key: "status"
+  },
+  {
+    title: "Akcja",
+    key: "action",
+    render: (text, record) => (
+      <span>
+        <a style={{ margin: "0 10px" }} href="#">
+          Wycofaj Aplikacje
+        </a>
+        <a style={{ margin: "0 10px" }} href="#">
+          Edytuj
+        </a>
+      </span>
+    )
+  }
+];
+
+const topicsData = [
+  {
+    key: "1",
+    name: "Przetwarzanie obrazów za pomocą biblioteki open CV",
+    applicants: 32
+  },
+  {
+    key: "2",
+    name: "Przetwarzanie obrazów za pomocą biblioteki open CV",
+    applicants: 12
+  },
+  {
+    key: "3",
+    name: "Przetwarzanie obrazów za pomocą biblioteki open CV",
+    applicants: 17
+  }
+];
+
+const applicationsData = [
+  {
+    key: "1",
+    topicName: "Przetwarzanie obrazów za pomocą biblioteki open CV",
+    positionName: "Python (OpenCV) Developer",
+    status: "Zaakceptowany"
+  },
+  {
+    key: "2",
+    topicName: "System dobierania studentów do prac inzynierskich",
+    positionName: "Programista Javy",
+    status: "Odrzucony"
+  },
+  {
+    key: "3",
+    topicName: "Rozproszone obliczenia",
+    positionName: "Python Developer",
+    status: "W trakcie przetwarzanie"
+  }
+];
 
 class Dashboard extends React.Component {
   render() {
@@ -11,13 +113,24 @@ class Dashboard extends React.Component {
           <h1>Witaj!</h1>
         </Header>
         <Content style={{ margin: "0 16px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
-            Index
-          </div>
+          <Row gutter={16}>
+            <Col className="gutter-row" span={12}>
+              <h2 style={{ margin: "20px 0" }}>Twoje Tematy</h2>
+              <Table
+                columns={topicColumns}
+                dataSource={topicsData}
+                pagination={false}
+              />
+            </Col>
+            <Col className="gutter-row" span={12}>
+              <h2 style={{ margin: "20px 0" }}>Twoje Aplikacje</h2>
+              <Table
+                columns={applyColumns}
+                dataSource={applicationsData}
+                pagination={false}
+              />
+            </Col>
+          </Row>
         </Content>
       </Layout>
     );
