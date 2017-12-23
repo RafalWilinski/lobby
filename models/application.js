9a48b3ff20601bcb7979a72f3b161b4003ce5e17
Rafal Wilinski <raf.wilinski@gmail.com>
2017-12-23 22:20:34 +0100

    Add Application model

---
+++ models/application.js
@@ -0,0 +1,29 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Application = sequelize.define("Application", {
+    roleId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    status: DataTypes.STRING,
+    description: DataTypes.STRING,
+    login: {
+      type: DataTypes.STRING
+      primaryKey: true,
+    }
+  });
+
+  Application.associate = models => {
+    Application.Role = Application.belongsTo(models.Role, {
+      foreignKey: "id",
+      sourceKey: "roleId"
+    });
+
+    Application.User = Application.belongsTo(models.User, {
+      foreignKey: "login",
+      sourceKey: "login"
+    });
+  }
+
+  return User;
+};
