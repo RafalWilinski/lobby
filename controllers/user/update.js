a3c745b67386b30eacbd5a1d87d72911f8c45304
DonKoszal <koszalpiotr@gmail.com>
2018-01-06 20:11:30 +0100

    Updated localStorage after profile update

--- controllers/user/update.js
+++ controllers/user/update.js
@@ -5,6 +5,10 @@ const update = async ctx => {
     const user = await User.update(ctx.request.body.user,
       { where: { login: ctx.request.body.user.login }}
     );
+
+    ctx.body = {
+      user: ctx.request.body.user
+    };
   } catch (err) {
     throw {
       statusCode: 400,
